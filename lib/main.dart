44516069b3146c6cacd067f04edfaf86eae65feb
Anish Shrestha <anishshrestha@tuicms.edu.np>
2023-07-28 16:06:08 +0545

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lottery App'),
+        ),
+        body: Column(
+          children: [],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.refresh_outlined),
+        ),
+      ),
+    );
+  }
+}
